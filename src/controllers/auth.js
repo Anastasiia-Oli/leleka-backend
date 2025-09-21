d2c42253e8d352b5261115173546807132374561
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-09-21 14:12:55 +0300

    login main

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 //
-import { registerUser } from '../services/auth.js';
+import { ONE_DAY } from '../constants/index.js';
+import { loginUser, registerUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -8,5 +9,26 @@ export const registerUserController = async (req, res) => {
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in a user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
-import {validateBody} from '../middlewares/validateBody.js';
-import { registerUserSchema } from '../validation/auth.js';
-import { registerUserController } from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserController,
+  registerUserController,
+} from '../controllers/auth.js';
 
 const authRouter = Router();
 
-authRouter.post('/register', validateBody(registerUserSchema), registerUserController);
+authRouter.post(
+  '/register',
+  validateBody(registerUserSchema),
+  registerUserController,
+);
+
+authRouter.post('/login', validateBody(loginUserSchema), loginUserController);
 
 export default authRouter;
