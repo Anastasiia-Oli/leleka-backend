17f611f795962080b64bc0185d33930a81b81afc
A_Oliinyk <aolejnik916@gmail.com>
2025-09-22 23:38:54 +0200

    fix documentation, folder name and paths

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,16 +1,28 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middlewares/auth.js';
-import { getCurrentUser, updateUserData, updateUserAvatar} from '../controllers/users.js';
+import {
+  getCurrentUser,
+  updateUserData,
+  updateUserAvatar,
+} from '../controllers/users.js';
 import { upload } from '../middlewares/multer.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { updateSchema } from '../validation/users.js';
 
-
 const usersRouter = Router();
 
-usersRouter.get('/me', authenticateToken, getCurrentUser);
-usersRouter.patch('/update', authenticateToken, validateBody(updateSchema),updateUserData);
-usersRouter.patch('/avatar', authenticateToken, upload.single('avatar'), updateUserAvatar);
-
+usersRouter.get('/current', authenticateToken, getCurrentUser);
+usersRouter.patch(
+  '/',
+  authenticateToken,
+  validateBody(updateSchema),
+  updateUserData,
+);
+usersRouter.patch(
+  '/avatar',
+  authenticateToken,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
 
 export default usersRouter;
