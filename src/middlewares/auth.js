41b5b2be542f818e2d250ebf6029fba777749dfb
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-09-22 21:01:37 +0300

    comented function generateToken

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,9 +3,9 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 
 const SECRET_KEY = getEnvVar('SECRET_KEY');
 
-export function generateToken(payload) {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-}
+// export function generateToken(payload) {
+//   return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+// }
 
 export function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
