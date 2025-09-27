ad650f21cea991d211496d59d8a90ba96581877f
A_Oliinyk <aolejnik916@gmail.com>
2025-09-27 18:46:18 +0200

    fix auth

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,22 +3,35 @@ import { UsersCollection } from '../db/models/users.js';
 import { SessionsCollection } from '../db/models/session.js';
 
 export const authenticate = async (req, res, next) => {
+  let token = null;
   // check authorization header
   const authHeader = req.get('Authorization');
 
-  if (!authHeader) {
-    next(createHttpError(401, 'Please provide Authorization header'));
-    return;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    token = authHeader.split(' ')[1];
   }
 
-  // check header type and token presence
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
-
-  if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer'));
-    return;
+  if (!token && req.cookies.accessToken) {
+    token = req.cookies.accessToken;
   }
+
+  if (!token) {
+    return next(createHttpError(401, 'Access token not provided'));
+  }
+
+  // if (!authHeader) {
+  //   next(createHttpError(401, 'Please provide Authorization header'));
+  //   return;
+  // }
+
+  // // check header type and token presence
+  // const bearer = authHeader.split(' ')[0];
+  // const token = authHeader.split(' ')[1];
+
+  // if (bearer !== 'Bearer' || !token) {
+  //   next(createHttpError(401, 'Auth header should be of type Bearer'));
+  //   return;
+  // }
 
   // check session presence
   const session = await SessionsCollection.findOne({ accessToken: token });
