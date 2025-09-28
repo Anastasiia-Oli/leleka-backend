bbcc4f75d3cddb9fd217bf286d0b5238ef40bce1
A_Oliinyk <aolejnik916@gmail.com>
2025-09-28 22:54:47 +0200

    try cloudinary

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,9 @@ export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,23 +1,27 @@
+// import multer from 'multer';
+// import path from "path";
+// import fs from "fs";
+
+// const __dirname = path.resolve();
+// const uploadDir = path.join(__dirname, "uploads");
+
+// if (!fs.existsSync(uploadDir)){
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadDir);
+//   },
+//   filename:(req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+// const ext = path.extname(file.originalname);
+// cb (null, file.fieldname + "-" + uniqueSuffix + ext);
+//   },
+// });
+
+// export const upload = multer({ storage});
+
 import multer from 'multer';
-import path from "path";
-import fs from "fs";
 
-const __dirname = path.resolve();
-const uploadDir = path.join(__dirname, "uploads");
-
-if (!fs.existsSync(uploadDir)){
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename:(req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-const ext = path.extname(file.originalname);
-cb (null, file.fieldname + "-" + uniqueSuffix + ext);
-  },
-});
-
-export const upload = multer({ storage});
+export const upload = multer({ dest: 'tmp/' });
