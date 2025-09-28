acae4ac480eb8c0929f7c07c42c640f8d334d505
A_Oliinyk <aolejnik916@gmail.com>
2025-09-28 18:50:14 +0200

    rewrite register service and controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,7 +3,22 @@ import { loginUser, registerUser, logoutUser } from '../services/auth.js';
 import { refreshUserSession } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
+  const { session, user } = await registerUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.cookie('accessToken', session.accessToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
 
   res.status(201).json({
     status: 201,
